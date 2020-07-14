d0c36ca10da0e643a574185cd944f45f47ccc524
nick.durbin <nick.durbin@gmail.com>
2020-07-14 19:31:23 -0400

    Added notes to the recursive binary search.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,15 +1,31 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # First we establish our middle point in the list/array
+    # We create a variable called middle
+    # Then, we add our start and end before dividing the floor by 2
     middle = (start + end) // 2
     
-    if len(arr) ==  0:
+    # We do a check for array length.
+    # IF it is 0, then we do not need to go further. 
+    if len(arr) == 0:
         return -1
 
+    # Similar notes on the regular binary search
+    # We first check if the middle value is our target
+    # If so, then simply return because we found it.
     if arr[middle] == target:
         return middle
+    # If the middle is greater than our target value
+    # We call the function and make our end value the middle value
+    # Because it is no longer needed.
+    # We can then continue to run iterations till we find out target.
     elif arr[middle] > target:
         return binary_search(arr, target, start, middle)
+    # If the middle is smaller than our target value
+    # We call the function and make our start value the middle value
+    # Because it is no longer needed.
+    # We can then continue to run iterations till we find out target.
     elif arr[middle] < target:
         return binary_search(arr, target, middle, high)
 
@@ -23,4 +39,4 @@ def binary_search(arr, target, start, end):
 def agnostic_binary_search(arr, target):
     # Your code here
     pass
-    
+
